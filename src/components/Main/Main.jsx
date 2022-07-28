6044522600b45c733657377c7715744fa13a4648
Benyamin428 <benyamin428@gmail.com>
2022-07-28 10:56:36 +0100

    line through text when checked

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,21 @@
 import Task from "../Task/Task";
+import { useState } from "react";
 
 const Main = ({tasks}) => {
 
+    const [checked, setChecked] = useState(false);
+
+    const setCheckbox = () => {
+        setChecked(!checked);
+    }
+
     const listOfTasks = tasks.map(task => {
-        return <Task key={task.id} taskDesc={task.description} />
+        return <Task 
+        key={task.id} 
+        taskDesc={task.description} 
+        checked={checked} 
+        setCheckbox={setCheckbox} 
+        />
     })
 
     return (
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,6 +1,11 @@
-const Task = ({taskDesc}) => {
+import "./Task.scss";
+
+const Task = ({taskDesc, checked, setCheckbox}) => {
     return (
-        <p>{taskDesc}</p>
+        <>
+            <input type="checkbox" id="task" onChange={setCheckbox} />
+            <label className={checked?"checked":"unchecked"}htmlFor="task"> {taskDesc}</label>
+        </>
     );
   }
   
